69043145f57ee6d4db6c501ad4cdc31a27e6e837
Ke Shen <kshen4@me.com>
2017-08-29 22:05:27 -0400

    modified statis

--- response.py
+++ response.py
@@ -45,9 +45,41 @@ attachments_json = [
 def message_options():
     form_json = json.loads(request.form["payload"])
     print(form_json)
+
+
+    attachments_json = [
+        {
+            "fallback": "你是痘痘肌吗？...",
+            "title": "你是痘痘肌吗？...",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "callback_id": "question_2",
+            "actions": [
+                {   
+                    "name": "1",
+                    "text": "是痘痘肌，想用对痘痘肌肤友好的产品",
+                    "value": "1",
+                    "type": "button",
+                },
+                {
+                    "name": "2",
+                    "text": "有痘印，想用遮瑕度高的产品",
+                    "value": "2",
+                    "type": "button",
+                },
+                {
+                    "name": "3",
+                    "text": "没有痘痘困扰",
+                    "value": "3",
+                    "type": "button",
+                }
+            ]
+        }
+    ]
+    
     slack_client.api_call(
       "chat.postMessage",
-      channel=channel,
+      channel=form_json["channel"]["id"],
       text="你是痘痘肌吗？",
       attachments=attachments_json
     )

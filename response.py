8e9fa4c7f9e2f686b51b225c15f30d4ce0d83dad
Ke Shen <kshen4@me.com>
2017-08-29 21:43:24 -0400

    modified statis

--- response.py
+++ response.py
@@ -1,10 +1,13 @@
+from flask import Flask, request, make_response, Response
+import os
 import json
-from bottle import route, run, request
 
-@route('/response', method='POST')
-def upload_image():
-    response = request.query.payload
-    print response
+app = Flask(__name__)
+
+@app.route("/response", methods=["POST"])
+def message_options():
+	form_json = json.loads(request.form["payload"])
+	print form_json
 
 if __name__ == "__main__":
-    run(host='0.0.0.0', port=8080, debug=True)
+    run(host='0.0.0.0', port=3500, debug=True)

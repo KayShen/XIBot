572cd57cc64145216612a074ef66dc507f7258ce
Ke Shen <kshen4@me.com>
2017-08-29 21:43:35 -0400

    modified statis

--- response.py
+++ response.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/response", methods=["POST"])
 def message_options():
 	form_json = json.loads(request.form["payload"])
-	print form_json
+	print(form_json)
 
 if __name__ == "__main__":
-    run(host='0.0.0.0', port=3500, debug=True)
+    app.run()

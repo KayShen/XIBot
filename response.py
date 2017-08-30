3eed16edc02e8600da8785ed056ce464a72a8f45
Ke Shen <kshen4@me.com>
2017-08-29 22:00:49 -0400

    modified statis

--- response.py
+++ response.py
@@ -4,28 +4,55 @@ import json
 import time
 from slackclient import SlackClient
 
-# slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+slack_client = 'xoxb-232306006676-RbKxztSArd46oopnAJFlE2Bn'
 
 app = Flask(__name__)
+
+attachments_json = [
+    {
+        "fallback": "你是痘痘肌吗？...",
+        "title": "你是痘痘肌吗？...",
+        "color": "#3AA3E3",
+        "attachment_type": "default",
+        "callback_id": "question_2",
+        "actions": [
+            {   
+                "name": "1",
+                "text": "是痘痘肌，想用对痘痘肌肤友好的产品",
+                "value": "1",
+                "type": "button",
+            },
+            {
+                "name": "2",
+                "text": "有痘印，想用遮瑕度高的产品",
+                "value": "2",
+                "type": "button",
+            },
+            {
+                "name": "3",
+                "text": "没有痘痘困扰",
+                "value": "3",
+                "type": "button",
+            }
+        ]
+    }
+]
+
 
 @app.route("/response", methods=["POST"])
 def message_options():
     form_json = json.loads(request.form["payload"])
     print(form_json)
-    menu_options = {
-        "options": [
-            {
-                "text": "Chess",
-                "value": "chess"
-            },
-            {
-                "text": "Global Thermonuclear War",
-                "value": "war"
-            }
-        ]
-    }
+    slack_client.api_call(
+      "chat.postMessage",
+      channel=channel,
+      text="你是痘痘肌吗？",
+      attachments=attachments_json
+    )
 
-    return Response(json.dumps(menu_options), mimetype='application/json')
-
+    # return Response(json.dumps(menu_options), mimetype='application/json')
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3500)

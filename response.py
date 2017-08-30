3c5ddc987526021110ec3cb43a982f7e7e5955fc
Ke Shen <kshen4@me.com>
2017-08-29 21:48:24 -0400

    modified statis

--- response.py
+++ response.py
@@ -1,13 +1,31 @@
 from flask import Flask, request, make_response, Response
 import os
 import json
+import time
+from slackclient import SlackClient
+
+# slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 app = Flask(__name__)
 
 @app.route("/response", methods=["POST"])
 def message_options():
-	form_json = json.loads(request.form["payload"])
-	print(form_json)
+    form_json = json.loads(request.form["payload"])
+
+    menu_options = {
+        "options": [
+            {
+                "text": "Chess",
+                "value": "chess"
+            },
+            {
+                "text": "Global Thermonuclear War",
+                "value": "war"
+            }
+        ]
+    }
+
+    return Response(json.dumps(menu_options), mimetype='application/json')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3500)
